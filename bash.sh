ad155ff1ea95c3066039bf3c12bceeea75e37e69
Web4 <167559384+Web4application@users.noreply.github.com>
2025-10-01 02:41:07 +0100

    Update and rename bash to bash.sh

--- bash.sh
+++ bash.sh
@@ -1,3 +1,12 @@
+
+# Make sure git-lfs is installed (https://git-lfs.com)
+git lfs install
+
+git remote add origin git@hf.co:Seriki/ProjectpilotAI
+
+# Make sure SSH key is set in your user settings (https://huggingface.co/settings/keys)
+git push -u origin main
+
 curl -SL https://github.com/docker/compose/releases/download/v2.37.3/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 
  Start-BitsTransfer -Source "https://github.com/docker/compose/releases/download/v2.37.3/docker-compose-windows-x86_64.exe" -Destination $Env:ProgramFiles\Docker\docker-compose.exe
